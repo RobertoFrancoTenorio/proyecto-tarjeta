774635abab897f2edea34b6ce2d4a7cf99b728bf
Roberto Carlos Franco Tenorio <robertofrancotenorio2@gmail.com>
2017-08-21 20:55:54 -0500

    Intercambio inicial de proyecto

---
+++ Tarjeta.java
@@ -0,0 +1,27 @@
+public class Tarjeta
+{
+    private float saldo;
+    public Tarjeta()
+    {
+        saldo = 0;
+    } 
+    public void depositar(float dinero)
+    {
+        saldo=saldo+dinero;
+    } 
+    public float retirar(float dinero)
+    {
+        if(saldo>=dinero)
+        {
+            saldo=saldo-dinero;
+            return(dinero);
+        }
+        else
+            return(0);
+    }
+    public void consultar()
+    {
+        System.out.println("El saldo es: ");
+        System.out.println(saldo);
+    } 
+} 
